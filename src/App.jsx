be2455c8548c477419ce6e1a552371afc73f37e0
Gustavo Faroni <116437253+GustavoFaroni@users.noreply.github.com>
2025-11-10 12:51:18 -0300

    vamo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Cards from './components/molecules/card'
+
+function App() {
+  return (
+    <>
+     <Cards />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/atoms/button.jsx
@@ -0,0 +1,13 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+    <Stack 
+    spacing={2} 
+    direction="row" 
+    >
+      <Button variant="contained">add to cart</Button>
+    </Stack>
+  );
+}
