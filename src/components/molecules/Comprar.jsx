6c463d179909e3acb4a27304936d0bacd8ae52fb
Gustavo Faroni <gustavofaroni@gmail.com>
2026-01-14 15:02:08 -0300

    removendo o local storage

--- src/components/molecules/Comprar.jsx
+++ src/components/molecules/Comprar.jsx
@@ -1,22 +1,15 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState(() => {
-    const salvoCarrinho = localStorage.getItem('carrinho');
-    return salvoCarrinho ? JSON.parse(salvoCarrinho) : [];
-  });
+  const [cartItems, setCartItems] = useState([]);
 
-  useEffect(() => {
-    localStorage.setItem('carrinho', JSON.stringify(cartItems));
-  }, [cartItems]);
 
-  
   const adicionarItem = (product) => {
     setCartItems((prevItems) => {
       const itemExists = prevItems.find((item) => item._id === product._id);
-      
+
       if (itemExists) {
         return prevItems.map((item) =>
           item._id === product._id
@@ -24,23 +17,22 @@ export const CartProvider = ({ children }) => {
             : item
         );
       }
-      
+
       return [...prevItems, { ...product, quantidade: 1 }];
     });
   };
 
-  
   const removerItem = (productId) => {
     setCartItems((prevItems) => prevItems.filter((item) => item._id !== productId));
   };
 
-  
+
   const attQuantidade = (productId, quant) => {
-    setCartItems((prevItems) => 
+    setCartItems((prevItems) =>
       prevItems.map((item) => {
         if (item._id === productId) {
-          const novaQuantidade = item.quantidade + quant;
-          return { ...item, quantidade: novaQuantidade > 0 ? novaQuantidade : 1 };
+        
+          return { ...item, quantidade: Math.max(1, item.quantidade + quant) };
         }
         return item;
       })
@@ -48,10 +40,28 @@ export const CartProvider = ({ children }) => {
   };
 
   
+  const limparCarrinho = () => {
+    setCartItems([]);
+  };
+
+
   const cartCount = cartItems.reduce((acc, item) => acc + item.quantidade, 0);
 
+
+  const cartTotal = cartItems.reduce((acc, item) => acc + (item.price * item.quantidade), 0);
+
   return (
-    <CartContext.Provider value={{ cartItems, adicionarItem, removerItem, attQuantidade, cartCount }}>
+    <CartContext.Provider 
+      value={{ 
+        cartItems, 
+        adicionarItem, 
+        removerItem, 
+        attQuantidade, 
+        limparCarrinho,
+        cartCount,
+        cartTotal 
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
